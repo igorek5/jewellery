516e567f00f597448975d9a94030265a8657caff
igorek5 <igorbautin101@gmail.com>
2022-02-27 12:18:07 +0300

    footer

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,21 @@
-// Swiper 7.4.1
-// import './vendor/swiper';
+/* global Swiper */
+/* Swiper 7.4.1 */
+
+import './vendor/swiper';
+
+const slider = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  grabCursor: true,
+  slidesPerGroup: 4,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: (index, className) => {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    },
+  },
+});
+
+slider();
